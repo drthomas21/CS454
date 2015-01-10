e2de39cacbb28b37ea0f3d99f202b97b209d4f3a
David Thomas <david.thomas214@yahoo.com>
2015-01-10 17:41:58 -0500

    adding assignment 1 partial

--- examples/functions.js
+++ examples/functions.js
@@ -10,7 +10,12 @@ var addOne = function (arg) {
 	return arg + 1;
 }
 
+var addZero = function addIDK (arg) {
+	return arg;
+}
+
 console.log(addThree(10));
 console.log(addTwo(10));
 console.log(addOne(10));
+console.log(addZero(10));
 
