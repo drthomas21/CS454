84323aca5e5f52e9147d044f01d8c377390fdf7b
David Thomas <david.thomas214@yahoo.com>
2015-01-10 23:43:32 -0500

    moving files to assignment1

--- assignments/assignment1/assignment1.js
+++ assignments/assignment1/assignment1.js
@@ -7,17 +7,19 @@ var reflection = function(string) {
 	
 	console.log("Question 1: " + response);
 };
+reflection('loop');
 
 //Question 2
 var pattern = function(start, end, spread) {
 	var response = [];
-	while(start < end) {
+	while(start <= end) {
 		response.push(start);
 		start += spread;
 	}
 	
 	console.log("Question 2: " + response.join(', '));
 };
+pattern(10,100,5);
 
 //Question 3
 var addAll = function(list) {
@@ -28,6 +30,7 @@ var addAll = function(list) {
 	
 	console.log("Question 3: " + response);
 };
+addAll([1,32,53,2,455,2,31,45,56,6,88,34]);
 
 //Question 4
 
